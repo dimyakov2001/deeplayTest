5e26e28570c5f8fa6ef907f19581ff30ffe0f53a
Дмитрий Яковенко <5430392-dimyakov@users.noreply.gitlab.com>
2022-06-04 19:17:53 +0600

    feat(src/main/java/Creature.java): creature class

---
+++ src/main/java/Creature.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.HashMap;
+
+public class Creature {
+    public HashMap<String, Integer> transportCosts;
+
+    public Creature() {
+        this.transportCosts = new HashMap<>();
+    }
+}
